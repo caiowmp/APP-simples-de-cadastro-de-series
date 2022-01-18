bfcabf26f9643bba333a8f678b828e9cc36459df
Caio <contato.caiomp@gmail.com>
2022-01-18 12:31:33 -0300

    Criação do ListarSerie e do AdicionarSeries

--- Program.cs
+++ Program.cs
@@ -1,15 +1,53 @@
 ﻿using System;
+using Projeto.Classes;
+using Projeto.Enum;
 
 namespace Projeto
 {
     class Program
     {
+        //Instanciando nosso banco de dados
+        static SerieRepositorio repositorio = new SerieRepositorio();
+
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            int escolha = Menu();
+
+            while (escolha != 0)
+			{
+				switch (escolha)
+				{
+					case 1:
+						ListarSeries();
+						break;
+					case 2:
+						AdicionarSerie();
+						break;
+					case 3:
+						//AtualizarSerie();
+						break;
+					case 4:
+						//ExcluirSerie();
+						break;
+					case 5:
+						//VisualizarSerie();
+						break;
+					case 6:
+						Console.Clear();
+						break;
+
+					default:
+						throw new ArgumentOutOfRangeException();
+				}
+
+				escolha = Menu();
+			}
+
+			Console.WriteLine("Obrigado!!");
+			Console.ReadLine();
         }
 
-        public static int Menu(){
+        private static int Menu(){
             Console.WriteLine();
 			Console.WriteLine("Bem vindo!!!");
 			Console.WriteLine("Escolha uma opção:");
@@ -26,6 +64,54 @@ namespace Projeto
 			int escolha = Convert.ToInt32(Console.ReadLine());
 			Console.WriteLine();
 			return escolha;
+        }
+
+        private static void ListarSeries()
+        {
+            Console.WriteLine("Listar séries");
+
+			var listaSeries = repositorio.Lista();
+
+            if(listaSeries.Count == 0)
+            {
+                System.Console.WriteLine("Nenhuma série cadastrada");
+            }
+            else
+            {
+                foreach(var item in listaSeries)
+                {
+                    System.Console.WriteLine("ID({0}) - {1}", item.getId(), item.getTitulo());
+                }
+            }
+
+        }
+
+        private static void AdicionarSerie()
+        {
+            System.Console.WriteLine();
+            System.Console.WriteLine("Adicionar nova série");
+
+            foreach(int chave in Genero.GetValues(typeof(Genero)))
+            {
+                System.Console.WriteLine("{0} - {1}", chave, Genero.GetName(typeof(Genero), chave));
+            }
+
+            System.Console.WriteLine("Escolha o gênero da série: ");
+            int chaveGenero = Convert.ToInt32(Console.ReadLine());
+
+            System.Console.WriteLine("Digite o título da série: ");
+            string titulo = Console.ReadLine();
+
+            System.Console.WriteLine("Digite o ano de início da série: ");
+            int ano = Convert.ToInt32(Console.ReadLine());
+
+            System.Console.WriteLine("Digite a descrição da série: ");
+            string descricao = Console.ReadLine();
+
+            Serie nova = new Serie(repositorio.ProximoId(),(Genero)chaveGenero,titulo,descricao,ano);
+
+            repositorio.Insere(nova);
+
         }
     }
 }
