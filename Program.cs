4cfa877bb96ff03230d1f0243d9949c0e68623dc
Caio <contato.caiomp@gmail.com>
2022-01-18 12:44:46 -0300

    Implementação dos métodos AtualizarSerie ExcluirSerie e VisualizarSerie

--- Program.cs
+++ Program.cs
@@ -24,13 +24,13 @@ namespace Projeto
 						AdicionarSerie();
 						break;
 					case 3:
-						//AtualizarSerie();
+						AtualizarSerie();
 						break;
 					case 4:
-						//ExcluirSerie();
+						ExcluirSerie();
 						break;
 					case 5:
-						//VisualizarSerie();
+						VisualizarSerie();
 						break;
 					case 6:
 						Console.Clear();
@@ -112,6 +112,60 @@ namespace Projeto
 
             repositorio.Insere(nova);
 
+        }
+
+        private static void AtualizarSerie()
+        {
+            System.Console.WriteLine();
+            System.Console.WriteLine("Atualizar série");
+        
+            System.Console.WriteLine("Digite o ID da série: ");
+            int id = Convert.ToInt32(Console.ReadLine());
+
+            foreach(int chave in Genero.GetValues(typeof(Genero)))
+            {
+                System.Console.WriteLine("{0} - {1}", chave, Genero.GetName(typeof(Genero), chave));
+            }
+
+            System.Console.WriteLine("Escolha o gênero da série: ");
+            int chaveGenero = Convert.ToInt32(Console.ReadLine());
+
+            System.Console.WriteLine("Digite o título da série: ");
+            string titulo = Console.ReadLine();
+
+            System.Console.WriteLine("Digite o ano de início da série: ");
+            int ano = Convert.ToInt32(Console.ReadLine());
+
+            System.Console.WriteLine("Digite a descrição da série: ");
+            string descricao = Console.ReadLine();
+
+            Serie atualizada = new Serie(repositorio.ProximoId(),(Genero)chaveGenero,titulo,descricao,ano);
+
+            repositorio.Atualiza(id,atualizada);
+        }
+
+        private static void ExcluirSerie()
+        {
+            System.Console.WriteLine();
+            System.Console.WriteLine("Excluir série");
+
+            System.Console.WriteLine("Digite o ID da série: ");
+            int id = Convert.ToInt32(Console.ReadLine());
+
+            repositorio.Exclui(id);
+        }
+
+        private static void VisualizarSerie()
+        {
+            System.Console.WriteLine();
+            System.Console.WriteLine("Vizualizar série");
+
+            System.Console.WriteLine("Digite o ID da série: ");
+            int id = Convert.ToInt32(Console.ReadLine());
+
+            var serie = repositorio.RetornaPorId(id);
+
+            System.Console.WriteLine(serie);
         }
     }
 }
